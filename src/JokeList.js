6080a1a1c076aa385d5f52b5aad70455e3ffc4f0
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-12 16:47:23 +0530

    Add styles to the layout and jokelist div

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './JokeList.css';
 
 class JokeList extends Component {
 	static defaultProps = {
@@ -31,7 +32,16 @@ class JokeList extends Component {
 	render() {
 		return (
 			<div className='JokeList'>
-				<h1>Dad jokes</h1>
+				<div className='JokeList-sidebar'>
+					<h1 className='JokeList-title'>
+						<span>Dad</span> Jokes
+					</h1>
+					<img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+					<button className='JokeList-getmore' onClick={this.handleClick}>
+						Fetch Jokes
+					</button>
+				</div>
+
 				<div className='JokeList-jokes'>
 					{this.state.jokes.map(j => (
 						<div>{j}</div>
