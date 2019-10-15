8c0787385ebee83b8f9609849da1410282f7d767
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-15 15:19:18 +0530

    Add local storage feature to save jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,11 +12,17 @@ class JokeList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			jokes: []
+			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
 		};
 	}
 
-	async componentDidMount() {
+	componentDidMount() {
+		if (this.state.jokes.length === 0) {
+			this.getJokes();
+		}
+	}
+
+	async getJokes() {
 		let jokes = [];
 
 		while (jokes.length < this.props.numJokesToGet) {
@@ -29,6 +35,7 @@ class JokeList extends Component {
 		this.setState({
 			jokes: jokes
 		});
+		window.localStorage.setItem('jokes', JSON.stringify(jokes));
 	}
 
 	handleVote(id, delta) {
